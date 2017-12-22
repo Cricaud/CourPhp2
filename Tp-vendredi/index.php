0bdcfea81366420ad2c841b8839684a6075c2a8d
Ashura <ashura@github.com>
2017-12-22 10:43:16 +0100

    Vendredi

--- Tp-vendredi/index.php
+++ Tp-vendredi/index.php
@@ -1,6 +1,20 @@
 <?php
-require_once "autoload.php";
-use FormBuilder\FormBuilder;
-$fb = new FormBuilder(); $form = $fb->buildContactForm();
+	require_once "autoload.php";
+
+	use FormBuilder\FormBuilder;
+
+	$fb = new FormBuilder();
+	$form = $fb->buildContactForm();
+
 ?>
-<!DOCTYPE html> <html lang="fr"> <head> <meta charset="UTF-8"> <title>FormBuilder</title> </head> <body> <?php $form->draw(); ?> </body> </html>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Form Builder</title>
+</head>
+<body>
+	<?php $form->draw(); ?>
+</body>
+</html>
